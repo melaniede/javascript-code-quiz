c5303d66295bc6ae9a114d56aea4b206490c2d49
melaniede <debarrosmelanie@gmail.com>
2020-07-19 19:43:06 -0400

    trying timer code

--- script.js
+++ script.js
@@ -45,3 +45,27 @@ const questions = [
        }
     ];
 
+// getElementById("startQuiz");
+let startQuizEl = document.querySelector("#test");
+startQuizEl.addEventListener("click", startQuizFunction);
+
+
+function startQuizFunction() {
+    var sec = 60;
+    function startTimer(){
+        console.log('timer suppose to go')
+        var timer = setInterval(function(){
+            sec--;
+            document.getElementById('timerDisplay').innerHTML='00:'+sec;
+            if (sec < 0) {
+                clearInterval(timer);
+                alert("Time is up!")
+            }
+        }, 1000);
+    }
+    document.getElementById('incorrect').addEventListener('click', function() {
+        sec -= 5;
+        document.getElementById('timerDisplay').innerHTML='00:'+sec;
+    });
+    startTimer();
+}
